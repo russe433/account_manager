6815f9ccbc4d37a671c11f0aa24b317bd34dbff2
russe433 <russe433@msu.edu>
2022-07-06 20:44:00 -0400

    added initial files and started projects

---
+++ routes/api/user-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const { User } = require('../../models');
+
+router.get('/', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.put('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
